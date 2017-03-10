c5d6ee82be337048d92753732c7ead13eaf64d05
Brian Edmunds <brian.edmundsjr@outlook.com>
2017-03-10 13:45:33 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,13 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne, cardTwo);
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if ((cardsInPlay.length === 2) && (cardOne === cardTwo)) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
